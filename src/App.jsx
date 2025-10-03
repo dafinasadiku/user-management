a83ec371971bb29fb51a7aec6c0149b8e93ce135
Dafina Sadiku <dafinaasadiku@gmail.com>
2025-10-03 18:33:44 +0200

    Update Home component style

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,7 @@ function App(){
   const[search, setSearch] = useState("");
   return (
     <div>
-      <div className="search">
-        <TextField id="textField" variant="outlined" fullWidth label="Search"
-        value={search} onChange={(e) => setSearch(e.target.value)} >
-        </TextField>
-      </div>
-      <div><Home search={search} /></div>
+      <div><Home/></div>
     </div>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
+import "./App.css";
+import TextField from '@mui/material/TextField';
 
-function Home({search}) {
+
+function Home() {
   const [users, setUsers] = useState([]);
+  const[search, setSearch] = useState("");
+  
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -9,31 +14,43 @@ function Home({search}) {
       .then((data) => setUsers(data));
   }, []);
 
-    const filteredUsers = users.filter((u) =>
+  const filteredUsers = users.filter((u) =>
     u.name.toLowerCase().includes(search.toLowerCase())
-);
+  );
 
   return (
     <div>
-      <h1>User Lists</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Company</th>
-          </tr>
-        </thead>
-        <tbody>
-           {filteredUsers.map((u) => (
+      <div
+        id="title"
+        style={{ fontFamily: "Arial, sans-serif", color: "rgb(0, 0, 48)" }}
+      >
+        <h1>User Management</h1>
+      </div>
+         <div className="search">
+                <TextField id="textField" variant="outlined" fullWidth label="Search"
+                value={search} onChange={(e) => setSearch(e.target.value)} >
+                </TextField>
+              </div>
+      <div className="table-container">
+        <table >
+          <thead>
             <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Company</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredUsers.map((u) => (
+              <tr>
                 <td>{u.name}</td>
                 <td>{u.email}</td>
                 <td>{u.company?.name}</td>
-            </tr>
-        ))}
-        </tbody>
-      </table>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
